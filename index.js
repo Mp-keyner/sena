8544e3311197d6001f0c4d37869814f16a9b6c27
Keyner De la Hoz <107326379+Mp-keyner@users.noreply.github.com>
2023-07-31 10:25:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const inputHorasExtras = document.getElementById('inputHorasExtras');
+  const opcSi = document.getElementById('opcSi');
+  const opcNo = document.getElementById('opcNo');
+
+  // Agregar evento para controlar la habilitación y deshabilitación del input de texto
+  opcSi.addEventListener('change', function() {
+    if (opcSi.checked) {
+      inputHorasExtras.removeAttribute('disabled');
+    } else {
+      inputHorasExtras.setAttribute('disabled', 'disabled');
+    }
+  });
+
+  opcNo.addEventListener('change', function() {
+    if (opcNo.checked) {
+      inputHorasExtras.setAttribute('disabled', 'disabled');
+    } else {
+      inputHorasExtras.removeAttribute('disabled');
+    }
+  });
 
